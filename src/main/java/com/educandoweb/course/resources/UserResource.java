585f5c7f2c543fe78efa259a6c5c29066fa36255
vinicius claudomir <viniciusclaudomir@gmail.com>
2022-06-18 14:25:57 -0300

    feature: create method and list all and method return one user

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,38 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.UserAccount;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource  {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<UserAccount> findAll() {
+	public ResponseEntity<List<UserAccount>> findAll() {
 		
-		UserAccount u = new UserAccount(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+		List<UserAccount> listWithAllUsers = userService.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(listWithAllUsers);
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<UserAccount> findById(@PathVariable Long id) {
+		
+		UserAccount UserProvideDatabase = userService.findByid(id);
+		
+		return ResponseEntity.ok().body(UserProvideDatabase);
 	}
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.UserAccount;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<UserAccount> findAll() {
+		
+		return repository.findAll();
+	}
+	
+	public UserAccount findByid(Long id) {
+		
+		Optional<UserAccount> userSelectedById = repository.findById(id);
+		
+		return userSelectedById.get();
+		
+	}
+	
+}
