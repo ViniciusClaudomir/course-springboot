c4ea0d20001888347957b0234df84de59e847c90
vinicius claudomir <viniciusclaudomir@gmail.com>
2022-06-18 22:00:50 -0300

    feature: create class category and methods to consume

---
+++ src/main/java/com/educandoweb/course/resources/CategoryResources.java
@@ -0,0 +1,38 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+
+ 		List<Category> listWithAllCategorys = categoryService.findAll();
+
+		return ResponseEntity.ok().body(listWithAllCategorys);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+
+		Category CategoryProvideDatabase = categoryService.findById(id);
+
+		return ResponseEntity.ok().body(CategoryProvideDatabase);
+	}
+}
